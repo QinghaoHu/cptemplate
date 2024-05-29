bc1cf8b02154c5d062085e9ccb24c76821fa8ed2
Qinghao Hu Mac <350681565@gapps.yrdsb.ca>
2024-05-29 14:06:02 -0400

    Update

---
+++ Sort/mergeSort/mergeSort.cpp
@@ -0,0 +1,35 @@
+//The header need for this template
+#include <iostream>
+#include <vector>
+using namespace std;
+using ll = long long;
+
+void mergeSort(int l, int s, vector<int> &arr) {
+    if (l >= s) return;
+    int mid = (l + s) >> 1;
+    mergeSort(l, mid, arr);
+    mergeSort(mid + 1, s, arr);
+    int i = l, j = mid + 1;
+    int cnt = 0; 
+    vector<int> c(s - l + 1, 0);
+    while(i <= mid && j <= s) {
+        if (arr[i] <= arr[j]) {
+            c[cnt++] = arr[i++];
+        } else {
+            c[cnt++] = arr[j++];
+        }
+    }
+    while(j <= s) {
+        c[cnt++] = arr[j++];
+    }
+    while(i <= mid) {
+        c[cnt++] = arr[i++];
+    }
+    for (int i = 0; i < cnt; i++) {
+        arr[l + i] = c[i];
+    }
+}
+
+/*
+* use Merge Sort to find the Inversion counts for a sequence. 
+*/
