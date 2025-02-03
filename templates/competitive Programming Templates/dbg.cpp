7174ad5b813db3d4205de9347a1ced94b9d12439
QinghaoHu <huqinghao@outlook.com>
2025-02-03 07:46:10 -0500

    update

---
+++ templates/competitive Programming Templates/dbg.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using std::cerr;
+
+template<typename T>
+void dbg(T x) {cerr << x << ' ';}
+template<typename T>
+void dbgl(T x) {cerr << x << '\n';}
+void dbg(bool x) {cerr << (!x ? "false" : "true") << ' ';}
+void dbgl(bool x) {cerr << (!x ? "false" : "true") << '\n';}
+template<typename T>
+void dbg(std::vector<T> u) {for(auto i : u) dbg(i);}
+template<typename T>
+void dbgl(std::vector<T> u) {for(auto i : u) dbgl(i); cerr << '\n';}
+
+int main() {
+	std::ios_base::sync_with_stdio(false), std::cin.tie(nullptr);
+
+    return 0;
+}
